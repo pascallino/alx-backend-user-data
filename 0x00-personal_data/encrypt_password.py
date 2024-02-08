c4cf6b214e6ad2678a1db4dab27914bee541b78c
pascallino <luvpascal.ojukwu@yahoo.com>
2024-02-08 17:42:04 +0100

    encrypt_password.py

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Defines a hash_password function to return a hashed password
+"""
+import bcrypt
+from bcrypt import hashpw
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Returns a hashed password
+    Args:
+        password (str): password to be hashed
+    """
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Check whether a password is valid
+    Args:
+        hashed_password (bytes): hashed password
+        password (str): password in string
+    Return:
+        bool
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
