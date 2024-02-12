b257e91e1fe098a80188114da984da81aba3f246
pascallino <luvpascal.ojukwu@yahoo.com>
2024-02-12 16:10:23 +0100

    api/v1/auth/basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,22 +9,23 @@ from typing import List, TypeVar
 class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Check if authentication is required for the given path."""
-        if path is None or excluded_paths is None or not excluded_paths:
+        if path is None:
             return True
-        if excluded_paths == []:
+        elif excluded_paths is None or excluded_paths == []:
             return True
-
-        if not path.endswith('/'):
-            path = path + '/'
-        for excluded_path in excluded_paths:
-            # Check if the path starts with an excluded path
-            # heck if it starts with a / , add it and compare
-            if not excluded_path.endswith('/'):
-                excluded_path = excluded_path + '/'
-            if path == excluded_path:
-                return False
-
-        return True   # Placeholder implementation, to be expanded later
+        elif path in excluded_paths:
+            return False
+        else:
+            for i in excluded_paths:
+                if i.startswith(path):
+                    return False
+                if path.startswith(i):
+                    return False
+                if i[-1] == "*":
+                    if path.startswith(i[:-1]):
+                        return False
+        return True
+        # Placeholder implementation, to be expanded later
 
     def authorization_header(self, request=None) -> str:
         """Extract the Authorization header from the request."""
